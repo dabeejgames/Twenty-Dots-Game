28d00a7a701b7b4d5d67ba068f013cd995ac1218
dabeejgames <dabeejgames@example.com>
2025-11-30 18:25:16 -0600

    Remove files not in local workspace

--- src/socket.js
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client";
-
-// Use your backend server URL and port
-const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:4000";
-export const socket = io(URL);
